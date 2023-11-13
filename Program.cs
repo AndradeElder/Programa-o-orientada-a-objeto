9cc46c35149a493ad166379398fae9ac6632ba17
elderandrade <elder_andrade@estudante.sesisenai.org.br>
2023-11-13 20:17:28 -0300

    exemplos classes e metodos

--- Program.cs
+++ Program.cs
@@ -1,2 +1,81 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//impressora, carro, pc, predio, celular
+
+    substantivos = propriedades
+    verbos = metodos
+
+    metodos pode ter retorno (concequencia)
+
+    //predio 
+         andares
+         elevador
+         numero
+         cor
+         endereço
+         material
+         nome
+
+         eventos
+
+         contruir
+         demolir
+         tipo de predio
+
+    // celular
+        propriedades
+
+        tipo de tela
+        processador
+        bateria
+        dimenções
+        sistema operacional
+        marca
+        modelo
+        cor
+
+        ---
+        metodos
+
+        ligar
+        desligar
+        telefonar
+        enviar mensagem
+        acessar internet
+    
+    //cachorro
+        propriedades
+        nome
+        raça
+        idade
+        periculosidade
+        peso
+        tutor
+    
+        metodos
+        morder
+        latir
+        correr
+        ficar velho
+
+    //carro
+        pneu
+        marca
+        motor
+        porta
+        volante
+        cambio
+        pedais
+
+        --metodos---
+        acelerar 
+        trocar marca
+        dirigir
+        abatecer
+        verificar peças
+
+
+
+// ------------- gerenciamento de condominio --------------------
+public class Condominio 
+{
+   // Fields, properties, methods and events go here...
+}
